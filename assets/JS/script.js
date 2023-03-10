a09c76a8b20bdf5d7d6c7d639a612f904f2dbe17
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-03-10 20:33:15 +0000

    score area

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -13,6 +13,9 @@ choiceBtns.forEach(button => button.addEventListener("click", () => {
     yourText.textContent = `Your choice: ${your}`;
     robotText.textContent = `Robot choice: ${robot}`;
     resultText.textContent = checkWinner();
+    if (resultText.textContent === "Congrats! You Win!"){incrementWin()};
+    if (resultText.textContent === "Draw!"){incrementDraw()};
+    if (resultText.textContent === "You Lose! Try Again!"){incrementLose()};
 }));
 
 function computerTurn(){
@@ -43,5 +46,29 @@ function checkWinner(){
     }
     else if(robot == "SCISSORS"){
       return (your == "ROCK") ? "Congrats! You Win!" : "You Lose! Try Again!"
-    }
+    };
+
+    
+}
+
+
+function incrementWin() {
+
+  let oldScore = parseInt(document.getElementById("win").innerText);
+  document.getElementById("win").innerText = ++oldScore;
+
+}
+
+function incrementDraw() {
+
+  let oldScore = parseInt(document.getElementById("draw").innerText);
+  document.getElementById("draw").innerText = ++oldScore;
+
+}
+
+function incrementLose() {
+
+  let oldScore = parseInt(document.getElementById("lose").innerText);
+  document.getElementById("lose").innerText = ++oldScore;
+
 }
