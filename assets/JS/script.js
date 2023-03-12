c9917ec87e323d2fb850fabb079fb9d75dd1462c
HoLaw77 <fh21hwl@leeds.ac.uk>
2023-03-12 21:42:19 +0000

    save

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -38,18 +38,16 @@ function computerTurn(){
 function checkWinner(){
     if(your == robot){
       return "Draw!";
-    } else
-    if(robot == "ROCK"){
+    } else if(robot == "ROCK"){
       return (your == "PAPER") ? "Congrats! You Win!" : "You Lose! Try Again!"
-    } else
-    if(robot == "PAPER"){
+    } 
+    else if(robot == "PAPER"){
       return (your == "SCISSORS") ? "Congrats! You Win!" : "You Lose! Try Again!"
-    } else
-    if(robot == "SCISSORS"){
+    } 
+    else if(robot == "SCISSORS"){
       return (your == "ROCK") ? "Congrats! You Win!" : "You Lose! Try Again!"
     } 
-   
-}
+   }
 
 
 function incrementWin() {
